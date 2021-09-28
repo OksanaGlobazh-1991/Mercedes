3067f9bca8ea7b99f79b55b1f304390c83d9be86
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-09-28 22:00:15 +0300

    sendForm is ready

---
+++ src/modules/sendForm.js
@@ -0,0 +1,40 @@
+const sendForm = () => {
+  const form = document.querySelectorAll('form');
+//'https://jsonplaceholder.typicode.com'
+
+
+form.forEach((item) => {
+  item.addEventListener('submit', (event) => {
+    event.preventDefault();
+  
+      let data = {}
+  
+      for (let { name, value } of item.elements) {
+        if (name) { //исключили баттон
+          data[name] = value;
+        }
+      }
+  
+      fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(data),
+      })
+        .then(response => {
+        if (response.status === 200 || response.status === 201) {
+          return response.json()
+        } else {
+          throw new Error(response.status)
+        }
+      })
+        .then(data => {
+            alert('Данные успешно сохранены!');
+            item.reset(); //очистили инпуты
+      })
+        .catch(error => {
+          alert('Ошибка отправки данных!');
+      })
+  })
+})
+};
+
+export default sendForm;
