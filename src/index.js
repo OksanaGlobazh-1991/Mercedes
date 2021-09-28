8617cb0cb7c1197b4a3bffd5021364deaa731ba4
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-09-28 22:02:56 +0300

    index is ready

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import accordeon from './modules/accordeon';
+import smoothScroll from './modules/smoothScroll';
+import tabs from './modules/tabs';
+import burger from './modules/burger';
+import modal from './modules/modal';
+import sendForm from './modules/sendForm';
+
+
+
+accordeon();
+smoothScroll();
+tabs();
+burger();
+modal();
+sendForm();
