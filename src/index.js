ce870e74c5f21c843f64cde5c416786a709ba816
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-09-28 23:16:18 +0300

    исправила название папки

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import accordeon from './modules/accordeon';
-import smoothScroll from './modules/smoothScroll';
-import tabs from './modules/tabs';
-import burger from './modules/burger';
-import modal from './modules/modal';
-import sendForm from './modules/sendForm';
+import accordeon from './module/accordeon';
+import smoothScroll from './module/smoothScroll';
+import tabs from './module/tabs';
+import burger from './module/burger';
+import modal from './module/modal';
+import sendForm from './module/sendForm';
 
 
 
